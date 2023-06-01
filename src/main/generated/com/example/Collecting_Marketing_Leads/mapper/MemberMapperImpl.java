b41f37a28254fd7a14a73009325f4efc3dadcbc6
KANG-MoonSoo <munsu2483@gmail.com>
2023-06-01 16:28:38 +0900

    swagger 연동

--- src/main/generated/com/example/Collecting_Marketing_Leads/mapper/MemberMapperImpl.java
+++ src/main/generated/com/example/Collecting_Marketing_Leads/mapper/MemberMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-30T03:30:17+0900",
+    date = "2023-06-01T15:58:04+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.15 (Azul Systems, Inc.)"
 )
 @Component
--- src/main/java/com/example/Collecting_Marketing_Leads/controller/MemberController.java
+++ src/main/java/com/example/Collecting_Marketing_Leads/controller/MemberController.java
@@ -4,6 +4,8 @@ import com.example.Collecting_Marketing_Leads.dto.MemberDto;
 import com.example.Collecting_Marketing_Leads.entity.Member;
 import com.example.Collecting_Marketing_Leads.mapper.MemberMapper;
 import com.example.Collecting_Marketing_Leads.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,6 +23,7 @@ import javax.validation.Valid;
 public class MemberController {
     private final MemberService memberService;
     private final MemberMapper memberMapper;
+
 
     @PostMapping
     public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post post){
